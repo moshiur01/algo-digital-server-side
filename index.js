b4d4f840e628800c325513d7d2e005036c492d34
moshiur01 <mr.moshiur314@gmail.com>
2021-12-26 21:00:13 +0600

    order delete by user done

--- index.js
+++ index.js
@@ -78,6 +78,18 @@ async function run() {
       const result = await ordersCollection.insertOne(order);
       res.json(result);
     });
+
+    //----------------------//
+    // delete area
+    //----------------------//
+
+    //delete order
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.json(result);
+    });
   } finally {
     // await client.close();
   }
