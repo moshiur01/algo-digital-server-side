1c9cce2e5cb9214a007013631bafb9b035438ef3
moshiur01 <mr.moshiur314@gmail.com>
2021-12-26 18:33:34 +0600

    order api added

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sx8wv.mongodb.net/algo-digital?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("algo-digital");
+    const servicesCollection = database.collection("services");
+    const teamCollection = database.collection("team-members");
+    const ordersCollection = database.collection("orders");
+
+    //----------------------//
+    // get area
+    //----------------------//
+
+    // get all services api
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    //get a specific service api
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await servicesCollection.findOne(query);
+      res.send(service);
+    });
+
+    // get team members api
+    app.get("/team", async (req, res) => {
+      const cursor = teamCollection.find({});
+      const team = await cursor.toArray();
+      res.send(team);
+    });
+
+    //----------------------//
+    // post area
+    //----------------------//
+
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await ordersCollection.insertOne(order);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("ALGO DIGITAL SERVER");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
