3ef7f9aaa5bd195d8419e7288d3c71929a027767
moshiur01 <mr.moshiur314@gmail.com>
2021-12-26 22:31:32 +0600

    manage services added

--- index.js
+++ index.js
@@ -134,6 +134,14 @@ async function run() {
       const result = await ordersCollection.deleteOne(query);
       res.json(result);
     });
+
+    // delete cars api
+    app.delete("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await servicesCollection.deleteOne(query);
+      res.json(result);
+    });
   } finally {
     // await client.close();
   }
