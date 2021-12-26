622dae789aa4f70586c63d0685b8186e61a7ce99
moshiur01 <mr.moshiur314@gmail.com>
2021-12-26 20:29:55 +0600

    specific order api generated

--- index.js
+++ index.js
@@ -61,6 +61,14 @@ async function run() {
       res.send(orders);
     });
 
+    // get a specific order api
+    app.get("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const order = await ordersCollection.findOne(query);
+      res.json(order);
+    });
+
     //----------------------//
     // post area
     //----------------------//
